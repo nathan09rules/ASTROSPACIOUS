7a15fc361feaf9444169f1d67b852127c972a0df
Nathan <dsouzanathan09@gmail.com>
2025-10-02 11:05:24 +0400

    major update

---
+++ js/main.js
@@ -0,0 +1,18 @@
+        const canvas = document.getElementById("stars");
+        const ctx = canvas.getContext("2d");
+
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        ctx.fillStyle = "midnightblue";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        for (let i = 0; i < 200; i++) {
+            let x = Math.random() * canvas.width;
+            let y = Math.random() * canvas.height;
+            let r = Math.random() * 1.5;
+            ctx.beginPath();
+            ctx.arc(x, y, r, 0, Math.PI * 2);
+            ctx.fillStyle = "white";
+            ctx.fill();
+        }
